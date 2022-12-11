8f5ff9cb98d5b13f9ab4c1f8d08b2d845b38e12a
Abdulrahman Abdulsalam <abdulrahmanabdulsalam69@gmail.com>
2022-12-11 04:41:04 -0800

    puts functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(void) 
-{	
-	puts("Programming is like building a multilingual puzzle\n");
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char test_str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(test_str);
 	return (0);
 }
