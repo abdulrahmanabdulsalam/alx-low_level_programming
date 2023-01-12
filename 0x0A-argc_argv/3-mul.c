a33d01f478cc5eeda5bcc0f44863980f52f4ea48
Abdulrahman Abdulsalam <abdulrahmanabdulsalam69@gmail.com>
2023-01-12 01:18:23 -0800

    3 mul code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,6 @@
 #include "main.h"
 
 
-
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
@@ -11,7 +10,6 @@
  */
 
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
@@ -22,9 +20,7 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;
 	while (s[len] != '\0')
-
 		len++;
-
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -44,14 +40,10 @@ int _atoi(char *s)
 		i++;
 
 	}
-
 	if (f == 0)
-
 		return (0);
-
 	return (n);
 }
-
 
 /**
  * main - multiplies two numbers
@@ -68,14 +60,11 @@ int main(int argc, char *argv[])
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
 	return (0);
 }
